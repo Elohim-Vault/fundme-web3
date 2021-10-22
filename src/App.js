d4aa263e478fc1625eb5feeda6f03b670b3ebfc4
unknown <gabrieljeronimo-@hotmail.com>
2021-10-22 18:32:40 -0300

    feat: Adding unit picker to donation form

--- src/App.js
+++ src/App.js
@@ -9,18 +9,18 @@ import FormControl from 'react-bootstrap/FormControl';
 
 
 function App() {
+  var web3 = new Web3(Web3.givenProvider);
   const [account, setAccount] = useState('');
   const [contract, setContract] = useState(null);
   const [amount, setAmount] = useState(0);
   const [balance, setBalance] = useState(0);
-
+  const [unit, setUnit] = useState('wei');
 
   useEffect(() => {
     loadBlockchainData();
   }, [])
 
   async function loadBlockchainData() {
-    const web3 = new Web3(Web3.givenProvider);
     const accounts = await web3.eth.getAccounts();
     const contractDonation = new web3.eth.Contract(DONATION_ABI, DONATION_ADDRESS);
     setAccount(accounts[0]);
@@ -30,9 +30,7 @@ function App() {
   }
 
   async function makeTransaction() {
-    contract.methods.fund().send({value: amount, from: account}).then(() => {
-
-    },);
+    contract.methods.fund().send({value: web3.utils.toWei(amount, unit), from: account});
   }
 
   return (
@@ -46,7 +44,15 @@ function App() {
             <Card.Title>Você pode contribuir com Ethereum!</Card.Title>
             <h1>Total já arrecadado: <span>{balance} wei</span></h1>
             <InputGroup className="mb-3">
-              <InputGroup.Text>Wei</InputGroup.Text>
+            <select
+                onChange={(e) => setUnit(e.target.value)}
+              >
+                <option defaultValue="wei">
+                  Wei
+                </option>
+                <option value="gwei">Gwei</option>
+                <option value="ether">Ether</option>
+              </select> 
               <FormControl onChange={e => setAmount(e.target.value)}/>
               <Button type="submit" onClick={makeTransaction}>Contribuir</Button>
             </InputGroup>
